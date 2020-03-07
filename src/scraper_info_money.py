702be4755c086f2b63a5f0f58e5cf93dad87fa05
Marcelo Wippel <mawippel2@hotmail.com>
2020-03-07 14:33:13 -0300

    feat: scraping info money and suno content

--- src/scraper_info_money.py
+++ src/scraper_info_money.py
@@ -14,11 +14,12 @@ def scrap():
     paragraphs = []
 
     get_news(hrefs, titles)
+    print(hrefs, titles)
     # for title in titles:
     #     news_path = BASE_PATH + title
     #     get_news_content(news_path, paragraphs)
     news_path = BASE_PATH + '/negocios/coronavirus-faz-empresas-perderem-bilhoes-em-valor-de-mercado-por-reducao-de-previsao-de-lucro/'
-    # get_news_content(news_path, paragraphs)
+    get_news_content(news_path, paragraphs)
     print(paragraphs)
 
 
@@ -28,30 +29,23 @@ def get_news(hrefs, titles):
     soup = BeautifulSoup(page.text, 'html.parser')
 
     htmlTitles = soup.findAll('div', {"id":re.compile("^post-")})
-    print(htmlTitles)
     for item in htmlTitles:
         txt_href = item.find('a')['href']
         txt_title = item.find('a').get('title')
+        hrefs.append(txt_href)
+        titles.append(txt_title)
 
 
 def get_news_content(news_path, paragraphs):
     """ Returns the paragraphs of the article """
     page = requests.get(news_path)
     soup = BeautifulSoup(page.text, 'lxml')
-    fullContent = soup.find(class_='article__content')
-
+    fullContent = soup.findAll('article', {"id":re.compile("^post-")})
     # Get the texts that are outside paragraphs
-    for without_paragraph_text in fullContent:
-        text = str(without_paragraph_text).strip()
-        # Ignore tags inside the text, in case it happens
-        if is_not_tag(text):
-            paragraphs.append(text)
-    fullContent = fullContent.find_all('p', recursive=False)
-
-    # Get the texts that are inside paragraphs
-    for i in range(0, len(fullContent) - 1):
-        text = fullContent[i].getText()
-        if is_not_script_tag(text):
+    for item in fullContent:
+        article_paragraphs = item.find_all('p')
+        for paragraph in article_paragraphs:
+            text = paragraph.getText()
             paragraphs.append(text)
 
 def is_not_script_tag(text):
--- src/scraper_suno.py
+++ src/scraper_suno.py
@@ -3,26 +3,47 @@ from bs4 import BeautifulSoup
 
 NEWS_PATH = 'https://www.sunoresearch.com.br/noticias/mercado/'
 
+
 def scrap():
-  print('Starting Suno Scraper...')
-  hrefs, titles = get_news()
-  print(hrefs, titles)
+    print('Starting Suno Scraper...')
+    hrefs, titles = get_news()
+    print(hrefs, titles)
+    # for href in hrefs:
+    #     get_news_content(href, paragraphs)
+    paragraphs = get_news_content('https://www.sunoresearch.com.br/noticias/ibovespa-queda-05022020/')
+    print(paragraphs)
+
 
 def get_news():
-  hrefs = []
-  titles = []
+    hrefs = []
+    titles = []
 
-  page = requests.get(NEWS_PATH)
-  soup = BeautifulSoup(page.text, 'html.parser')
-  
-  htmlTitles = soup.find_all(class_='list-item')
-  for item in htmlTitles:
-    title = item.find('h3', {'class':'post__title'})
-    txt_href = title.find('a')['href']
-    txt_title = title.find('a').text
-    hrefs.append(txt_href)
-    titles.append(txt_title)
-  return hrefs, titles
+    page = requests.get(NEWS_PATH)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    htmlTitles = soup.find_all(class_='list-item')
+    for item in htmlTitles:
+        title = item.find('h3', {'class': 'post__title'})
+        txt_href = title.find('a')['href']
+        txt_title = title.find('a').text
+        hrefs.append(txt_href)
+        titles.append(txt_title)
+    return hrefs, titles
+
+
+def get_news_content(news_path):
+    """ Returns the paragraphs of the article """
+    paragraphs = []
+    page = requests.get(news_path)
+    soup = BeautifulSoup(page.text, 'lxml')
+    fullContent = soup.find('div', itemprop="articleBody")
+    # Get the texts that are outside paragraphs
+    article_paragraphs = fullContent.find_all('p')
+    for paragraph in article_paragraphs:
+        text = paragraph.getText()
+        paragraphs.append(text)
+    return paragraphs
+
 
 if __name__ == "__main__":
     scrap()
