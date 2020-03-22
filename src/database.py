411c8e551a51b649ce9ca036d2ca010496ffa399
Marcelo Wippel <mawippel2@hotmail.com>
2020-03-21 21:53:43 -0300

    feat: persisting paragraphs

--- src/database.py
+++ src/database.py
@@ -1,9 +1,12 @@
 import sqlalchemy as db
 from model.news import News
 import yaml
+import uuid
+from datetime import datetime
 
 # Load the config file into memory
 config = yaml.safe_load(open("config.yml"))
+
 
 class Database():
     user = config['DB_USER']
@@ -18,10 +21,21 @@ class Database():
         self.connection = self.engine.connect()
         print("DB Instance created")
 
-    def save_data(self, news):
+    def save(self, news):
+        self.save_news(news)
+        self.save_paragraphs(news)
+
+    def save_news(self, news):
         self.connection.execute(f"""INSERT INTO news(id, title, href, created_at) 
                                     VALUES('{news.id}', '{news.title}', '{news.href}', '{news.created_at}')""")
 
+    def save_paragraphs(self, news):
+        for paragraph in news.paragraphs:
+            self.connection.execute(f"""INSERT INTO paragraphs(id, text, news_id, created_at)
+                                        VALUES('{str(uuid.uuid4())}', '{paragraph}', '{news.id}', '{datetime.now()}')""")
+
+
 if __name__ == "__main__":
     db = Database()
-    db.save_data(News('título de teste', 'https://aefaef.com.br'))
+    db.save(News('título de teste', 'https://aefaef.com.br',
+                      ['paragrafo 1', 'paragrafo 2', 'paragrafo 3']))
--- src/model/news.py
+++ src/model/news.py
@@ -2,8 +2,9 @@ import uuid
 from datetime import datetime
 
 class News():
-    def __init__(self, title, href):
+    def __init__(self, title, href, paragraphs):
         self.id = str(uuid.uuid4())
         self.title = title
         self.href = href
+        self.paragraphs = paragraphs
         self.created_at = datetime.now()
