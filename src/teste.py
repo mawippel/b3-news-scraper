331747e8120e114715673be8cd54edb4c43d5142
Marcelo Wippel <mawippel2@hotmail.com>
2020-05-05 20:25:42 -0300

    removendo arquivo lixo

--- src/teste.py
+++
@@ -1,7 +0,0 @@
-import locale
-from datetime import datetime
-
-locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
-
-datetime_object = datetime.strptime('5 maio 2020, 14h21', '%d %B %Y, %Hh%M')
-print(datetime_object)
